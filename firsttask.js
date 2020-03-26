9dd41aee5541545277a5c051d4a626af5041eb8c
Fadhil <fadhil.akins@gmail.com>
2020-03-26 16:58:24 +0100

    error corrected

--- firsttask.js
+++ firsttask.js
@@ -6,13 +6,13 @@ console.log(courses);
 
 if (courses.length % 2 == 0) {
     for (let i = 1; i <= 200; i++) {
-        if (i % 2 !== 0) {
+        if (i % 2 == 0) {
          console.log(i);
         }
     }
 } else {
     for (let i = 1; i <= 200; i++) {
-        if (i % 2 == 0) {
+        if (i % 2 !== 0) {
           console.log(i);
         }
     }
